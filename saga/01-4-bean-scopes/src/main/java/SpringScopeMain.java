2faba9d2e6ad5018c570b18443715167ccf4b0d9
Avramovic Slavisa <avramovic.slavisa@partake.de>
2018-03-01 09:51:45 +0100

    renaming main class in scope module

--- saga/01-4-bean-scopes/src/main/java/SpringScopeMain.java
+++ saga/01-4-bean-scopes/src/main/java/SpringScopeMain.java
@@ -7,7 +7,7 @@ import rs.saga.businessobject.IGame;
  * @author <a href="mailto:slavisa.avramovic@escriba.de">avramovics</a>
  * @since 2018-01-15
  */
-public class GlavnaSpring {
+public class SpringScopeMain {
 
     public static void main(String[] args) {
         ApplicationContext context =

a8c60e1db21c8707450cba7c844eeaff73ceb135
Avramovic Slavisa <avramovic.slavisa@partake.de>
2018-03-09 11:09:33 +0100

    03-01 1 basic aspect with joinpoint

---
+++ saga/03-01-aspects/src/test/java/rs/saga/service/GameIT.java
@@ -0,0 +1,49 @@
+package rs.saga.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import rs.saga.businessobject.ITeam;
+import rs.saga.configurationmetada.GameConfig;
+import rs.saga.dao.ITeamRepo;
+import rs.saga.dao.TeamNotFoundException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author <a href="mailto:slavisa.avramovic@escriba.de">avramovics</a>
+ * @since 2018-03-08
+ */
+@ContextConfiguration(classes = GameConfig.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@ActiveProfiles("dev")
+public class GameIT {
+
+    @Autowired
+    private ITeam home;
+    @Autowired
+    private ITeam away;
+    @Autowired
+    private Game gameUnderTest;
+    @Autowired
+    private ITeamRepo repo;
+
+    @Test
+    public void testUpdateSuccess() {
+        // setting up data
+        repo.save(home);
+
+        ITeam updated = gameUnderTest.update(home);
+        assertEquals("Partizan", updated.name());
+    }
+
+    @Test(expected = TeamNotFoundException.class)
+    public void testUpdateFailure() {
+        gameUnderTest.update(home);
+    }
+}

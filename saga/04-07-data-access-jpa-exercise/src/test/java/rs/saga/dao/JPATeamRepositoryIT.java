6e41c1fc8fd4db370224b7a16f9fe247083090e8
Avramovic Slavisa <avramovic.slavisa@partake.de>
2018-03-18 14:54:56 +0100

    04-7 - jpa example and exercise

--- saga/04-07-data-access-jpa-exercise/src/test/java/rs/saga/dao/JPATeamRepositoryIT.java
+++ saga/04-07-data-access-jpa-exercise/src/test/java/rs/saga/dao/JPATeamRepositoryIT.java
@@ -1,12 +1,15 @@
 package rs.saga.dao;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author <a href="mailto:slavisa.avramovic@escriba.de">avramovics</a>
  * @since 2018-03-15
  */
-public class HibernateTeamRepositoryIT {
+@RunWith(SpringRunner.class)
+public class JPATeamRepositoryIT {
     @Test
     public void save() throws Exception {
     }

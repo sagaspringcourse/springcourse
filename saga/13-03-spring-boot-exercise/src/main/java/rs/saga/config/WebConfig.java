46ec9f2a1a7eaa1c33bd1bad0deb0af7e4ebbb0b
Avramovic Slavisa <avramovic.slavisa@partake.de>
2018-05-07 08:57:32 +0200

    13-03 - spring boot exercise

---
+++ saga/13-03-spring-boot-exercise/src/main/java/rs/saga/config/WebConfig.java
@@ -0,0 +1,102 @@
+package rs.saga.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
+import org.springframework.web.servlet.theme.CookieThemeResolver;
+import org.springframework.web.servlet.theme.ThemeChangeInterceptor;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
+
+@Configuration
+@EnableWebMvc
+@EnableAspectJAutoProxy
+@ComponentScan(basePackages = {"rs.saga.web"})
+public class WebConfig implements WebMvcConfigurer {
+    //Declare our static resources. I added cache to the java config but it?s not required.
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/").setCachePeriod(31556926);
+        registry.addResourceHandler("/styles/**").addResourceLocations("/styles/").setCachePeriod(31556926);
+    }
+
+    // <=> <mvc:default-servlet-handler/>
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    // Serves up cached and compressed static content at /resources/* from the webapp root and classpath
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/auth").setViewName("auth");
+        registry.addViewController("/cancel").setViewName("cancel");
+    }
+
+
+    @Bean
+    InternalResourceViewResolver getViewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/");
+        resolver.setSuffix(".jsp" );
+        resolver.setRequestContextAttribute("requestContext");
+        return resolver;
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+        registry.addInterceptor(themeChangeInterceptor());
+        registry.addInterceptor(webChangeInterceptor());
+    }
+
+    @Bean
+    LocaleChangeInterceptor localeChangeInterceptor() {
+        return new LocaleChangeInterceptor();
+    }
+
+    @Bean
+    ThemeChangeInterceptor themeChangeInterceptor() {
+        return new ThemeChangeInterceptor();
+    }
+
+    @Bean
+    WebContentInterceptor webChangeInterceptor() {
+        WebContentInterceptor webContentInterceptor = new WebContentInterceptor();
+        webContentInterceptor.setCacheSeconds(0);
+        webContentInterceptor.setSupportedMethods("GET", "POST", "PUT", "DELETE");
+        return webContentInterceptor;
+    }
+
+    @Bean
+    CookieLocaleResolver localeResolver() {
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+        cookieLocaleResolver.setCookieMaxAge(3600);
+        cookieLocaleResolver.setCookieName("locale");
+        return cookieLocaleResolver;
+    }
+
+    @Bean
+    CookieThemeResolver themeResolver() {
+        CookieThemeResolver cookieThemeResolver = new CookieThemeResolver();
+        cookieThemeResolver.setDefaultThemeName("green");
+        cookieThemeResolver.setCookieMaxAge(3600);
+        cookieThemeResolver.setCookieName("theme");
+        return cookieThemeResolver;
+    }
+}
